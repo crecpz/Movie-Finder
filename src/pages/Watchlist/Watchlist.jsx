4a71ebc333286fd03540438e7a0a69a3c9732e9a
Yuan <crecpz@gmail.com>
2023-01-03 11:28:49 +0800

    [修正] watchlist__status-btns 小錯誤修正

--- src/pages/Watchlist/Watchlist.jsx
+++ src/pages/Watchlist/Watchlist.jsx
@@ -1,17 +1,40 @@
-import { useEffect,useState } from "react";
+import { useEffect, useState } from "react";
 import { NavLink, Outlet, useParams } from "react-router-dom";
 import { capitalize } from "../../utils/function";
 import ScrollToTop from "react-scroll-to-top";
+import { useRef } from "react";
 
 const Watchlist = ({ setUnreadList }) => {
   // 網址參數
   const { currentWatchStatus = "unwatched" } = useParams();
   // 設定 watchlist__status-btn 的閃爍狀態(在使用者改變觀看狀態時，閃爍按鈕)
   const [statusBtnFlashing, setStatusBtnFlashing] = useState(false);
+  // 存放 2 個 statusBtn 的 DOM
+  const statusBtnRefs = useRef([]);
   useEffect(() => {
     // 進入 watchlist 頁面後，將 unreadList 清空
     setUnreadList([]);
   }, []);
+
+  useEffect(() => {
+    let animationId;
+    // 處理有關 `.flashing-animation` class 的新增
+    const handleFlashingAnimationClass = (idx) => {
+      statusBtnRefs.current[idx].classList.remove("flashing-animation");
+      animationId = window.requestAnimationFrame(() => {
+        statusBtnRefs.current[idx].classList.add("flashing-animation");
+      });
+    };
+    if (statusBtnFlashing) {
+      // 根據目前所在的頁面位置，來決定哪一個 watchlist__status-btn 要閃爍
+      if (currentWatchStatus === "unwatched") {
+        handleFlashingAnimationClass(1);
+      } else {
+        handleFlashingAnimationClass(0);
+      }
+    }
+    return () => cancelAnimationFrame(animationId);
+  }, [statusBtnFlashing]);
 
   return (
     <section className="watchlist">
@@ -24,25 +47,18 @@ const Watchlist = ({ setUnreadList }) => {
                 <NavLink
                   key={index}
                   to={`/watchlist/${listType}`}
-                  className={({ isActive }) => {
-                    if (isActive || listType === currentWatchStatus) {
-                      return "watchlist__status-btn active";
-                    } else {
-                      // 根據 statusBtnFlashing 來控制按鈕閃爍(註:按鈕閃爍只會發生在非 active 的按鈕上)
-                      if (statusBtnFlashing) {
-                        return "watchlist__status-btn flashing-animation";
-                      } else {
-                        return "watchlist__status-btn";
-                      }
-                    }
-                  }}>
+                  ref={(el) => (statusBtnRefs.current[index] = el)}
+                  className={({ isActive }) =>
+                    isActive || listType === currentWatchStatus
+                      ? "watchlist__status-btn active"
+                      : "watchlist__status-btn"
+                  }>
                   {capitalize(listType)}
                 </NavLink>
               );
             })}
           </div>
         </div>
-
         <Outlet context={[statusBtnFlashing, setStatusBtnFlashing]} />
       </div>
       {/* ScrollToTop */}
